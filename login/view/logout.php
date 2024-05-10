bb407798c7a1a92a629114a7b3c5a7c01e885230
R3zaJafari <90835907+R3zaJafari@users.noreply.github.com>
2024-05-10 04:34:31 +0330

    Update logout.php and fix it

--- login/view/logout.php
+++ login/view/logout.php
@@ -1,3 +1,7 @@
+<?php
+  session_start();
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -5,5 +9,22 @@
     <title>Document</title>
 </head>
 <body>
-    
+    <div class="bg-green-500 m-2 p-2 rounded">
+      
+      <?php
+        if(isset($_SESSION['email']) || isset($_COOKIE['email'])){
+          echo "you are success fully logged in ". "<a class='text-white'>".$_COOKIE['email'] ."</a>"." you can log out now ";
+          echo "<br><br>";
+          echo "<a href='logout.php?logout=true' class='bg-red-500 text-white m-2 p-2 rounded'>logout</a>";
+        }
+
+        if(isset($_REQUEST['logout']))
+        {
+          unset($_COOKIE['email']); 
+          setcookie('email', '', -1, '/'); 
+          session_destroy();
+          header('location:../view/signUp.view.php');
+        }
+      ?>
+    </div>
 </body>
