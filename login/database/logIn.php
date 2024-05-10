e9dbf123b56f73fa65c9e7bbc84eb0a9360f11a0
R3zaJafari <90835907+R3zaJafari@users.noreply.github.com>
2024-05-10 04:36:10 +0330

    Update logIn.php fix log in

--- login/database/logIn.php
+++ login/database/logIn.php
@@ -3,56 +3,19 @@
 
         protected function getUser($email, $password)
         {
-            $query = $this->conn()->prepare('SELECT password FROM users WHERE email = ? ;');
-            
-            
-            if(!$query->execute(array($email)))
-            {
-                $query = null ;
-                header("location: ../view/logIn.view.php?error=wrong query");
-                exit();
-            }
-            
+            $query = $this->conn()->prepare("SELECT * FROM users WHERE email = :email AND password = :password;");
+            $statment = $query->execute(array("email"=> $email,"password"=> $password));
 
-            $loginData = $query->fetchAll(PDO::FETCH_ASSOC);
-            if(count($loginData) == 0) {
-            $stmt = null;
-            header("location: login.php?error=usernotfound");
-            exit();
-            }
+            $count = $query->rowCount();
 
-            $pwd_hashed = $query->fetchAll(PDO::FETCH_ASSOC);
-            
-
-            if(!$pwd_hashed == $password)
-            {
-                $query = null ;
-                header("location: ../view/logIn.view.php?error=wrong password");
-                exit();
-            }elseif($pwd_hashed == $password)
-            {
-                $query = $this->conn()->prepare('SELECT * FROM users WHERE email = ? AND password = ? ;');
-
-                if(!$query->execute(array($email, $pwd_hashed[0]['password'])))
-                {
-                    $query = null ;
-                    header("location: ../view/logIn.view.php?error=wrong query");
-                    exit();
-                }
-
-                if($query->rowCount() == 0)
-                {
-                    $query = null ;
-                    header("location: ../view/logIn.view.php?error=user not found");
-                    exit();
-                }
-
-                $user = $query->fetchAll(PDO::FETCH_ASSOC);
+            if ($count > 0) {
                 session_start();
-                $_SESSION['email'] = $user[0]['email'];
+                setcookie('email' , $email, time() + (86400 * 30), "/");
+                $_SESSION['email']  = $email;
+                header('location:../view/logout.php');
+            }else{
+                header('location:../view/login.view.php?error=user not found');
             }
-            
-            $query = null;
         }
 
         
